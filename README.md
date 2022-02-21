f417f83d790b5899d615e18d15a02925cae49533
谭文 <tanwen@thejoyrun.com>
2022-02-21 11:22:34 +0800

    A: 新增回到顶部组件

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ npm i run-components-all --S
 
 **使用**
 
-在mian.js中
+在 mian.js 中全局引入插件
 
 import RunAll from 'run-components-all';
 
@@ -14,10 +14,9 @@ Vue.use(RunAll);
 
 **例：**
 
-map的使用
-直接在template标签内使用
+map 的使用
+直接在 template 标签内使用组件
 
 <tableColumn></tableColumn>
 
 其中需要传入一些参数，详情以后补充，暂时可以先看源码
-
--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-
 // 图例
 // import Legend from './map_module/maptool/legend';
 // 所有的工具
@@ -21,20 +20,18 @@
 // import MapType from './map_module/maptool/mapType';
 // 鹰眼
 // import OverViewMap from './map_module/maptool/overviewMap';
-import TableColumn from './map_module/tableColumn/index';
-import NoticTip from './map_module/notic/notic';
+import TableColumn from "./map_module/tableColumn/index";
+import NoticTip from "./map_module/notic/notic";
+import ScrollTop from "./map_module/scrollTop/index";
 
-var components = [
-  TableColumn,
-  NoticTip
-];
-const install = function(Vue) {
-  components.map(function(component) {
+var components = [TableColumn, NoticTip, ScrollTop];
+const install = function (Vue) {
+  components.map(function (component) {
     Vue.component(component.name, component);
   });
 };
 
 export default {
   ...components,
-  install
+  install,
 };
